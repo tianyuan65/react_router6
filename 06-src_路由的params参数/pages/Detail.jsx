4386e1c04df3ecd156bfa2db648e6533c4adeca6
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2023-04-23 22:03:02 +0900

    some modification

--- 06-src_路由的params参数/pages/Detail.jsx
+++ 06-src_路由的params参数/pages/Detail.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { useParams,useMatch } from 'react-router-dom'
 
 export default function Detail() {
+    // useParams()中可以直接获取params参数
     const {id,title,content}=useParams()
+    // useMatch()钩子中需要传入完整的路径，包括占位的id、title及content，但用的不多，一般用useParams
     const match=useMatch('home/message/detail/:id/:title/:content')
     console.log(match);
     return (
