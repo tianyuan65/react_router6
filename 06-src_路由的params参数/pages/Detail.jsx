d6d70829ddc54deae29d762cbfcb8b26e618b358
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2023-04-23 17:07:19 +0900

    params parameter

---
+++ 06-src_路由的params参数/pages/Detail.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useParams,useMatch } from 'react-router-dom'
+
+export default function Detail() {
+    const {id,title,content}=useParams()
+    const x=useMatch('home/message/detail/:id/:title/:content')
+    console.log(x);
+    return (
+        <ul>
+            <li>{id}</li>
+            <li>{title}</li>
+            <li>{content}</li>
+        </ul>
+    )
+}
